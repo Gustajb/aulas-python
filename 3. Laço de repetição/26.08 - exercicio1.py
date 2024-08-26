baf0b06512c5fb6db36643a6ca25e39c59976a8d
Gustajb <gustavobatistagb09@gmail.com>
2024-08-26 19:57:58 +0000

    Aula 26.08

--- 3. Laço de repetição/26.08 - exercicio1.py
+++ 3. Laço de repetição/26.08 - exercicio1.py
@@ -5,9 +5,12 @@ os.system("cls || clear")
 
 pares = 0
 impares = 0
+soma = 0
 
 for i in range(5):
     numero = int(input("Digite um número: "))
+    soma = soma + numero
+    print(f"Soma: {soma}")
 
     if numero % 2 == 0:
         pares = pares + 1
@@ -16,3 +19,5 @@ for i in range(5):
 
 print(f"Quantidade de pares: {pares}")
 print(f"Quantidade de ímpares: {impares}")
+
+
--- 3. Laço de repetição/26.08 - exercicio2.py
+++ 3. Laço de repetição/26.08 - exercicio2.py
@@ -3,15 +3,19 @@ os.system("cls || clear")
 
 soma = 0
 
-for i in range (4):
-    nota = float(input("Digite um número: "))
+for i in range (3):
+    nota = float(input("Digite uma nota: "))
+    soma = soma + nota
 
-soma = soma + nota
-
-
-media = soma / 4
+media = soma / 3
 print(f"Média: {media}")
 
+if media >= 7:
+    print("Aprovado.")
+elif media < 4: # else if.
+     print("Reprovado.")
+else: 
+      print("Recuperação.")
 
 
 
